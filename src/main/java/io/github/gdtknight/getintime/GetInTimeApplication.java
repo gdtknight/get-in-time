c306b74a16e87ae76dce267c7a5979c31760b808
yongho shin <gdtknight@naver.com>
2023-04-13 22:02:28 +0900

    init project

---
+++ src/main/java/io/github/gdtknight/getintime/GetInTimeApplication.java
@@ -0,0 +1,13 @@
+package io.github.gdtknight.getintime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetInTimeApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(GetInTimeApplication.class, args);
+  }
+  
+}
---
+++ src/test/java/io/github/gdtknight/getintime/GetInTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gdtknight.getintime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetInTimeApplicationTests {
+  
+  @Test
+  void contextLoads() {
+  }
+  
+}
