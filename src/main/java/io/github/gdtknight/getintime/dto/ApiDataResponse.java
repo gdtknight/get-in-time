aa67393c771b988fd5463a889a202f24aba72441
yongho shin <gdtknight@naver.com>
2023-04-19 19:14:29 +0900

    Add #1 - error code, exception, api responses
    프로젝트에서 사용할 에러 코드와 예외, 공통 응답 객체 설계
    예외를 직접 정의하고 사용하는 이유는 프로젝트에서 예상한 문제와 예상치 못한 문제를 분리시키기 위해서임

---
+++ src/main/java/io/github/gdtknight/getintime/dto/ApiDataResponse.java
@@ -0,0 +1,21 @@
+package io.github.gdtknight.getintime.dto;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode(callSuper = true)
+public class ApiDataResponse extends ApiErrorResponse{
+  private final Object data;
+  
+  private ApiDataResponse(boolean success, Integer errorCode, String message, Object data) {
+    super(success, errorCode, message);
+    this.data = data;
+  }
+  
+  public static ApiDataResponse of(boolean success, Integer errorCode, String message, Object data) {
+    return new ApiDataResponse(success, errorCode, message, data);
+  }
+}
